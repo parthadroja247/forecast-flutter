1887fefe4361e32a4fd00e23db6855e1324667fc
Parth Adroja <parthadroja247@gmail.com>
2020-06-03 14:40:07 +0530

    Launch Screen Complete

--- lib/screens/launch_screen.dart
+++ lib/screens/launch_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:forecastflutter/services/location_service.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:location/location.dart';
 import 'package:shimmer/shimmer.dart';
 
 class LaunchScreen extends StatelessWidget {
   static const String id = 'launch_screen';
+
+  void getLocation(BuildContext context) async {
+    LocationData location = await LocationService().getCurrentLocation();
+    if (location != null) {
+      // Go to home
+      print(location.latitude);
+      print(location.longitude);
+    } else {
+      // Error fetching location
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getLocation(context);
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Expanded(
+            child: Container(),
+          ),
           Center(
             child: Image(
               image: AssetImage('images/launch_screen/app_logo.png'),
@@ -31,6 +49,31 @@ class LaunchScreen extends StatelessWidget {
               ),
             ),
           ),
+          Expanded(
+            child: Container(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Made with ❤️ ',
+                style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.white.withOpacity(0.5)),
+              ),
+              Text(
+                'Parth Adroja',
+                style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white.withOpacity(0.5)),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          )
         ],
       ),
     );
