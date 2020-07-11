273594ca9e366299e18ad4f2b67a6d298578489b
Parth Adroja <padroja@mobiquityinc.com>
2020-07-11 11:54:54 +0530

    Rename home to main

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Text('HOME SCREEN'),
+        child: Text('Main SCREEN'),
       ),
     );
   }
